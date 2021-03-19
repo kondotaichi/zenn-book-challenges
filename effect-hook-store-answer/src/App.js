ecaffc35599b5e975e156aca93c9f34a8caf4084
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-03-20 00:40:16 +0100

    fix: price state to calculate correct with an alternative direct number value

--- effect-hook-store-answer/src/App.js
+++ effect-hook-store-answer/src/App.js
@@ -8,17 +8,31 @@ import { css, jsx } from '@emotion/react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [price, setPrice] = useState(200);
 
   useEffect(() => {
-    // Apply DOM later
-    console.log(`${count} times clicked!`);
+    const numbers = document.getElementById('numbers');
+    numbers.innerHTML = `${count} packs`;
   }, [count]);
+
+  useEffect(() => {
+    const prices = document.getElementById('prices');
+    prices.innerHTML = `¥${price}`;
+  });
 
   const color = '#454343';
 
+  const ReviewWrapper = styled.div`
+    background-color: #ffffff;
+  `
+
+  const CartWrapper = styled.div`
+    background-color: #F7F2F2;
+  `
+
   const ItemReview = styled.img`
     max-width: 80%;
-    margin: 8rem auto 1rem;
+    margin: 2rem;
     border-radius: 5px;
   `;
 
@@ -30,28 +44,70 @@ function App() {
     font-size: 1.5rem;
   `
 
+  // output differents results
+  // if buttun is clicked, run setCount(*default value is 1) and setPrice
+  // else show a component with default 0 package & ¥0
+
+  const ItemImg = styled.img`
+    max-width: 20%;
+    margin: 2rem;
+    border-radius: 5px;
+  `
+
+  const Item = styled.h3`
+    font-size: 1.7rem;
+  `
+
   return (
     <div className="App">
-      <ItemReview src={itemImg} alt="itemLogo" />
-      <ItemName css={css`
-        color: ${color};
-      `}>
-        Swag Sticker Pack
-      </ItemName>
-      <Description css={css`
-        color: ${color};
-      `}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <br />Sed pharetra consequat diam. In metus risus, aliquam non massa tempus,
-        <br />gravida commodo orci. Praesent interdum pharetra quam eu pharetra.
-      </Description>
-      <button onClick={() => setCount(count + 1)}>
-        +
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-        -
-      </button>
-      <h3>{count} packs</h3>
+      <ReviewWrapper>
+        <ItemReview src={itemImg} alt="itemLogo" />
+        <ItemName css={css`
+          color: ${color};
+        `}>
+          Swag Sticker Pack
+        </ItemName>
+        <Description css={css`
+          color: ${color};
+        `}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <br />Sed pharetra consequat diam. In metus risus, aliquam non massa tempus,
+          <br />gravida commodo orci. Praesent interdum pharetra quam eu pharetra.
+        </Description>
+        <button onClick={() => {
+          setCount(count + 1);
+          setPrice(200 * (count + 1));
+
+          console.log(`count: ${count}`);
+          console.log(`price: ${price}`);
+          console.log(`200 * (count + 1): ${200 * (count + 1)}`);
+        }}>
+          +
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        <h3>¥200</h3>
+        <h3>Quantity: {count} packs</h3>
+      </ReviewWrapper>
+
+      <CartWrapper>
+        <ItemName css={css`
+          color: ${color};
+        `}>
+          Cart 🛍
+        </ItemName>
+        <ItemImg src={itemImg} alt="itemImg" />
+        <Item
+          css={css`
+          color: ${color};
+        `}
+        >
+          Swag Stickers Pack
+        </Item>
+        <h3 id="numbers" />
+        <h3 id="prices" />
+      </CartWrapper>
     </div>
   );
 }
